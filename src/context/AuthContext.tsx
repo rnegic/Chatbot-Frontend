e8c2270c93a060fdeca57fcbf855f4249260b1f1
rnegic <arnego666@gmail.com>
2024-07-23 17:30:40 +0300

    AuthContext added

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { useContext, createContext, useEffect, useState, ReactNode } from "react";
+
+type User = {
+    name: string;
+    email: string;
+}
+
+type UserAuth = {
+    isSignedin: boolean;
+    user: User | null;
+    signin: (email: string, password: string) => Promise<void>;
+    signup: (name: string, email: string, password: string) => Promise<void>;
+    signout: () => Promise<void>;
+}
+const AuthContext = createContext<UserAuth | null>(null);
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isSignedin, setIsSignedin] = useState(false);
+
+    useEffect(() => {
+
+    }, []);
+    const signin = async (email: string, password: string) => {
+
+    };
+    const signup = async (name: string, email: string, password: string) => { };
+    const signout = async () => { };
+
+    const value = {
+        user,
+        isSignedin,
+        signin,
+        signout,
+        signup
+    };
+    return <AuthContext.Provider value={value}> {children} </AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
